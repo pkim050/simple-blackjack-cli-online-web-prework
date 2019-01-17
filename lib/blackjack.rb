ce46707396e8dcbe972bd8408c30c7b78ce5985d
Patrick Kim <pkim050@gmail.com>
2019-01-17 23:53:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -67,8 +67,8 @@ def runner
   welcome
   card_total = initial_round
   until card_total > 21
-    display_card_total(card_total)
     card_total = hit?(card_total)
+    display_card_total(card_total)
   end
   end_game(card_total)
 end
